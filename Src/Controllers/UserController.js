7a269f760d5a88429fc3d762a9535b61be3a465a
FernanPro18 <fernandoaraujo081@gmail.com>
2018-06-10 11:24:14 -0500

    Traductor

--- Src/Controllers/UserController.js
+++ Src/Controllers/UserController.js
@@ -1,14 +1,43 @@
 'use strict'
 const _Client = require('../Controllers/Firebase');
+const translate = require('google-translate-api');
 
+//Conseguir usuario por Id
 function GetUser(req, res){
   _Client.auth().getUser(req.params.Id)
   .then(function(userRecord) {
-    console.log("Successfully fetched user data:", userRecord.toJSON());
+    res.status(500).send('User: '+userRecord.toJSON());
   })
   .catch(function(error) {
-    console.log("Error fetching user data:", error);
+    translate(error.errorInfo.message, {to: 'es'}).then(Response => {
+      res.status(400).send(Response.text);
+    }).catch(err => {
+      res.status(400).send(error.errorInfo.message);
+    });
   });
 }
 
-module.exports = {GetUser}
+//Crear Usuario nuevo
+function CreateUser(req, res) {
+  _Client.auth().createUser({
+    email: req.body.Email,
+    emailVerified: false,
+    phoneNumber: req.body.Celular,
+    password: req.body.Password,
+    displayName: req.body.Nombre,
+    disabled: false
+  })
+  .then(function(userRecord) {
+    // See the UserRecord reference doc for the contents of userRecord.
+    res.status(500).send('User Creado: '+userRecord.uid);
+  })
+  .catch(function(error) {
+    translate(error.errorInfo.message, {to: 'es'}).then(Response => {
+      res.status(400).send(Response.text);
+    }).catch(err => {
+      res.status(400).send(error.errorInfo.message);
+    });
+  });
+}
+
+module.exports = {GetUser, CreateUser}
--- Src/Routes/Routes.js
+++ Src/Routes/Routes.js
@@ -4,4 +4,5 @@ var User = require('../Controllers/UserController');
 
 module.exports = function (app){
   app.get('/User/Get/:Id', User.GetUser);
+  app.post('/User/Create', User.CreateUser);
 }
