e5bca5eb1fa0ef1e984c04a6061a25bf0d11ff45
FernanPro18 <fernandoaraujo081@gmail.com>
2018-06-12 14:59:23 -0500

    Falta Login

--- Src/Controllers/UserController.js
+++ Src/Controllers/UserController.js
@@ -3,18 +3,19 @@ const _Client = require('../Controllers/Firebase');
 const translate = require('google-translate-api');
 
 //Conseguir usuario por Id
-function GetUser(req, res){
+function GetUser(req, res) {
+  //if(_Client.auth.)
   _Client.auth().getUser(req.params.Id)
-  .then(function(userRecord) {
-    res.status(500).send('User: '+userRecord.toJSON());
-  })
-  .catch(function(error) {
-    translate(error.errorInfo.message, {to: 'es'}).then(Response => {
-      res.status(400).send(Response.text);
-    }).catch(err => {
-      res.status(400).send(error.errorInfo.message);
+    .then(function (userRecord) {
+      res.status(200).send('User: ' + JSON.stringify({ displayName: userRecord.displayName, phoneNumber: userRecord.phoneNumber, email: userRecord.email}));
+    })
+    .catch(function (error) {
+      translate(error.errorInfo.message, { to: 'es' }).then(Response => {
+        res.status(400).send(Response.text);
+      }).catch(err => {
+        res.status(400).send(error.errorInfo.message);
+      });
     });
-  });
 }
 
 //Crear Usuario nuevo
@@ -27,17 +28,17 @@ function CreateUser(req, res) {
     displayName: req.body.Nombre,
     disabled: false
   })
-  .then(function(userRecord) {
-    // See the UserRecord reference doc for the contents of userRecord.
-    res.status(500).send('User Creado: '+userRecord.uid);
-  })
-  .catch(function(error) {
-    translate(error.errorInfo.message, {to: 'es'}).then(Response => {
-      res.status(400).send(Response.text);
-    }).catch(err => {
-      res.status(400).send(error.errorInfo.message);
+    .then(function (userRecord) {
+      // See the UserRecord reference doc for the contents of userRecord.
+      res.status(200).send('User Creado: ' + userRecord.uid);
+    })
+    .catch(function (error) {
+      translate(error.errorInfo.message, { to: 'es' }).then(Response => {
+        res.status(400).send(Response.text);
+      }).catch(err => {
+        res.status(400).send(error.errorInfo.message);
+      });
     });
-  });
 }
 
 //Actualiza Usuario
@@ -50,32 +51,62 @@ function UpdateUser(req, res) {
     displayName: req.body.Nombre,
     disabled: false
   })
-  .then(function(userRecord) {
-    // See the UserRecord reference doc for the contents of userRecord.
-    res.status(500).send('User Actualizado: '+userRecord.toJSON());
-  })
-  .catch(function(error) {
-    translate(error.errorInfo.message, {to: 'es'}).then(Response => {
-      res.status(400).send(Response.text);
-    }).catch(err => {
-      res.status(400).send(error.errorInfo.message);
+    .then(function (userRecord) {
+      // See the UserRecord reference doc for the contents of userRecord.
+      res.status(200).send('User Actualizado: ' + JSON.stringify({ displayName: userRecord.displayName, phoneNumber: userRecord.phoneNumber, email: userRecord.email }));
+    })
+    .catch(function (error) {
+      translate(error.errorInfo.message, { to: 'es' }).then(Response => {
+        res.status(400).send(Response.text);
+      }).catch(err => {
+        res.status(400).send(error.errorInfo.message);
+      });
     });
-  });
 }
 
 //Actualiza Usuario
 function DeleteUser(req, res) {
   _Client.auth().deleteUser(req.params.Id)
-  .then(function(userRecord) {
-    // See the UserRecord reference doc for the contents of userRecord.
-    res.status(500).send('User Eliminado');
-  })
-  .catch(function(error) {
-    translate(error.errorInfo.message, {to: 'es'}).then(Response => {
-      res.status(400).send(Response.text);
-    }).catch(err => {
-      res.status(400).send(error.errorInfo.message);
+    .then(function (userRecord) {
+      // See the UserRecord reference doc for the contents of userRecord.
+      res.status(200).send('User Eliminado');
+    })
+    .catch(function (error) {
+      translate(error.errorInfo.message, { to: 'es' }).then(Response => {
+        res.status(400).send(Response.text);
+      }).catch(err => {
+        res.status(400).send(error.errorInfo.message);
+      });
     });
-  });
 }
-module.exports = {GetUser, CreateUser}
+
+//Login
+function Login(req, res) {
+  _Client.auth().getUserByEmail(req.body.Email)
+    .then(function (userRecord) {
+      if(userRecord.Password == req.body.Password){
+        res.status(200).send("Hola");
+      }
+      res.status(200).send(userRecord.passwordHash+"    "+userRecord.passwordSalt);
+    })
+    .catch(function (error) {
+      translate(error.errorInfo.message, { to: 'es' }).then(Response => {
+        res.status(400).send(Response.text);
+      }).catch(err => {
+        res.status(400).send(error.errorInfo.message);
+      });
+    });
+}
+
+//Salir
+function SignOut(req, res) {
+  firebase.auth().signOut()
+    .catch(function (error) {
+      translate(error.errorInfo.message, { to: 'es' }).then(Response => {
+        res.status(400).send(Response.text);
+      }).catch(err => {
+        res.status(400).send(error.errorInfo.message);
+      });
+    });
+}
+module.exports = { GetUser, CreateUser, UpdateUser, DeleteUser, Login, SignOut }
--- Src/Routes/Routes.js
+++ Src/Routes/Routes.js
@@ -5,6 +5,7 @@ var User = require('../Controllers/UserController');
 module.exports = function (app){
   app.get('/User/Get/:Id', User.GetUser);
   app.post('/User/Post', User.CreateUser);
+  app.post('/User/Login', User.Login);
   app.put('/User/Put/:Id', User.UpdateUser);
-  app.Delete('/User/Delete/:Id', User.DeleteUser);
+  app.delete('/User/Delete/:Id', User.DeleteUser);
 }
